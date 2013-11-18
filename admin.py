f4f46e14f618202e58525afc193878ffca12f2d2
André Rendeiro <afrendeiro@gmail.com>
2013-11-18 10:29:54 +0100

    updated models

--- admin.py
+++ admin.py
@@ -1,21 +1,21 @@
 import models
 from django.contrib import admin
 
-class CategoryAdmin(admin.ModelAdmin):
+class NotebookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
-class CategoryToEntryInline(admin.TabularInline):
-    model = models.CategoryToEntry
-    extra = 1
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
 
 class EntryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     exclude = ('author',)
-    inlines = [CategoryToEntryInline]
 
     def save_model(self, request, obj, form, change):
         obj.author = request.user
         obj.save()
 
+
+admin.site.register(models.Notebook, NotebookAdmin)
 admin.site.register(models.Entry, EntryAdmin)
 admin.site.register(models.Category, CategoryAdmin)
--- models.py
+++ models.py
@@ -1,13 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Notebook(models.Model):
+	"""Notebook in which entries are.
+		Ideally corresponds to a project"""
+	title = models.CharField(max_length=200)
+	start_date = models.DateTimeField()
+	description = models.TextField(max_length=1000)
+	slug = models.SlugField(max_length=40, unique=True)
+	author = models.ForeignKey(User)
+
+	class Meta:
+		verbose_name_plural = "Notebooks"
+
+	def __unicode__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return "/notebooks/%s/" % self.slug
+
+
 class Entry(models.Model):
+	""" Labnotebook entry"""
 	title = models.CharField(max_length=200)
 	pub_date = models.DateTimeField()
 	text = models.TextField()
 	slug = models.SlugField(max_length=40, unique=True)
 	author = models.ForeignKey(User)
+	notebook = models.ForeignKey(Notebook)
 
 	def __unicode__(self):
 		return self.title
@@ -16,22 +38,20 @@ class Entry(models.Model):
 		return "/%s/%s/%s/" % (self.pub_date.year, self.pub_date.month, self.slug)
 
 	class Meta:
-	    ordering = ["-pub_date"]
+		ordering = ["-pub_date"]
+		verbose_name = _('Entry')
+		verbose_name_plural = _('Entries')
 
 class Category(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=40, unique=True)
-    description = models.TextField()
+	title = models.CharField(max_length=200)
+	slug = models.SlugField(max_length=40, unique=True)
+	description = models.TextField()
 
-    class Meta:
-        verbose_name_plural = "Categories"
+	class Meta:
+		verbose_name_plural = "Categories"
 
-    def __unicode__(self):
-        return self.title
+	def __unicode__(self):
+		return self.title
 
-    def get_absolute_url(self):
-        return "/categories/%s/" % self.slug
-
-class CategoryToEntry(models.Model):
-    entry = models.ForeignKey(Entry)
-    category = models.ForeignKey(Category)
+	def get_absolute_url(self):
+		return "/categories/%s/" % self.slug
